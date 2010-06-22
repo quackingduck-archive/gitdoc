aa60b2033ce894a620c125ec628d2854cdcc1046
Myles Byrne <myles@myles.id.au>
2010-06-22 17:07:12 -0500

    v 1.0

--- documentinator.rb
+++ documentinator.rb
@@ -2,10 +2,32 @@ require 'sinatra'
 require 'rdiscount'
 
 def Documentinator!
-  set :dir, File.dirname(File.expand_path(caller.first.split(':').first))
+  dir = File.dirname(File.expand_path(caller.first.split(':').first))
+  set :dir, dir
+  set :styles, dir + '/styles.sass'
   run Sinatra::Application
 end
 
+set :haml, {:format => :html5}
+set :views, lambda { root }
+
+helpers do
+
+  def md source
+    RDiscount.new(source).to_html.
+    # uses newline entity in pre tags
+    gsub(/<code>.*?<\/code>/m) do |match|
+      match.gsub(/\n/m,"&#x000A;")
+    end
+  end
+
+  def styles
+    sass File.read(settings.styles) if File.exist? settings.styles
+  end
+
+end
+
 get '/' do
-  RDiscount.new(File.read(settings.dir + '/index.md')).to_html
+  @doc = md File.read(settings.dir + '/index.md')
+  haml :doc
 end
